6d94cf435acebec4b4bb096d9be23f2cf7f08393
Ilene Cohen <ilene413@icloud.com>
2018-11-24 15:11:08 -0500

    working - before crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,45 +9,87 @@ $(document).ready(function () {
     let playerScore = 0;
     let crystalValue;
     let crystalImages = ["alexandrite-gemstone", "amber gems", "pink star gem", "ruby gem"];
+    let buttonFile = "assets/images/";
+    let buttonPicture;
+    let buttonImg;
 
-
-
+    console.log("before start game and clic");
     startGame();
-    $(".crystal-button").on("click", function () {
+    console.log("after first start game");
+    $("#crystals").on("click", ".crystal-button", function () {
+
+        // get value of button clicked and add to players score
+        // save players score in a holder so that can update player score dynamically
+        console.log("crystal clicked");
         crystalValue = $(this).attr("data-crystal");
-        console.log ("crystal value is" + crystalValue);
+        console.log("crystal value is" + crystalValue);
         crystalValue = parseInt(crystalValue);
         playerScore = playerScore + crystalValue;
-        console.log ("players Score is " + playerScore);
-        $("#playerScore").append("Your Score: " + playerScore);
+        console.log("players Score is " + playerScore);
+        $("#playerScore").text("Your Score:  " + playerScore);
+        if (playerScore < randomNumber) {
+            return;
+        }
+        else {
+            if (playerScore === randomNumber) {
+                alert("You Win" + playerScore);
+                numWins++;
+                console.log("out of start game win");
+            }
+            else {
+                alert("You Lose" + playerScore);
+                numLoss++;
+                console.log("out of start game lose");
+            }
+            startGame();
+            console.log('back from start game after game over');
+        }
     });
 
     //This function is called when starting a new game
-    //sets players score to zero, gets new random numbers for random number and crytals, and hides the crystal's value
+    //clears previous game, gets new random numbers for random number and crytals
 
     function startGame() {
-        // get random numbers
+        // clear all variables
+        $("#matchRandomNumber, #numWins, #numLosses, #playerScore, #crystals").empty();
+
+        // get random number to match
         randomNumber = Math.floor(Math.random() * 102 + 19);
 
-        // Assign random numbers for the user to match and to the crystals
+        //players score is set to 0
+        playerScore = 0;
+
+        // Assign random numbers to crystals
         for (var i = 0; i < numCrystals; i++) {
             let crystalBtn = $("<button>");
-            crystalBtn.addClass("crystal-button crystal crystal-button-color");
+            crystalBtn.addClass("crystal-button crystal crystal-button-image");
             crystalValue = Math.floor(Math.random() * 12 + 1);
             crystalBtn.attr("data-crystal", crystalValue);
-            crystalBtn.text(crystalValue);
-        //    crystalBtn.attr("hidden", true);
-            
+            console.log("crystal button i " + crystalValue);
+            buttonPicture = buttonFile + crystalImages[i] + (".jpg");
+            //           $(".crystal-button-image").css("background-image", "url('/assets/images/ruby gem.jpg')");
+            //            crystalBtn.attr("crystal-button-immage", buttonPicture);
+
+            console.log("button picture is " + buttonPicture);
+            //$( "crystalBtn" ).text( $( "buttonPicture" ).attr( crystalImages[i]));
+            crystalBtn.text(buttonPicture);
+            // display crystals    
             $("#crystals").append(crystalBtn);
+            //            $("#crystals").append(crystalBtn).append("<img src=buttonPicture height=100 width=100/>").button();
         }
-
+        console.log("out of for loop");
+        //        displayGame();
+        //   }
+        //   function displayGame() {
         // display new game board dynamically    
-        $("#matchRandomNumber").append("Random Number: " + randomNumber);
-        $("#numWins").append("Wins:" + numWins);
+        $("#matchRandomNumber").append("Random Number:  " + randomNumber);
+        console.log('after match random');
+        $("#numWins").append("Wins:  " + numWins);
+        console.log("after numwins");
         $("#numLosses").append("Losses: " + numLoss);
-        $("#playerScore").append("Your Score: " + playerScore);
+        console.log('after  num losses');
+        $("#playerScore").append("Your Score:  " + playerScore);
+        console.log("after player score");
     }
-
-
 
 });
